5081b8c8f428329d4b5af33e254bf094684c2a4c
daniisaila <isaila.dan@yahoo.com>
2022-08-11 22:46:16 +0300

    Releases order solved and design improvements

--- src/app/version-list/version-list-convertor/releases-convertor.service.ts
+++ src/app/version-list/version-list-convertor/releases-convertor.service.ts
@@ -28,7 +28,45 @@ export class ReleaseConvertorService
             let releasesString:string = JSON.stringify(data);
             let jsonfile = JSON.parse(releasesString)
             this.releaseVersions = jsonfile.releasedVersionsList;
-            
+    
+            const compareSemanticVersions = (va: Version, vb: Version) => {
+ 
+                let a:string = va.versionNumber.toString();
+                let b:string = vb.versionNumber.toString();
+
+                // 1. Split the strings into their parts.
+                const a1 = a.split('.');
+                const b1 = b.split('.');
+                // 2. Contingency in case there's a 4th or 5th version
+                const len = Math.min(a1.length, b1.length);
+                // 3. Look through each version number and compare.
+
+                console.log(a1);
+                console.log(b1);
+
+                for (let i = 0; i < len; i++) {
+                
+                    const a2 = +a1[ i ] || 0;
+                    console.log(a2);
+                    const b2 = +b1[ i ] || 0;
+                    console.log(b2);
+                    
+                    if (a2 !== b2) {
+                        return a2 > b2 ? 1 : -1;        
+                    }
+                }
+                
+                // 4. We hit this if the all checked versions so far are equal
+                //
+                console.log("ceva: " + (b1.length - a1.length));
+                return a1.length - b1.length;
+            };
+    
+            const versionss = [ '1.1.0', '5.0.0', '0.2.0', '2.4.1', '1.02.1' ];
+            const sorted = this.releaseVersions.sort(compareSemanticVersions);
+            sorted.reverse();
+            console.log(sorted);
+
             this._VersionsSubject.next(this.releaseVersions);
             console.log(this.releaseVersions);
 
--- src/app/version-list/version-list.component.ts
+++ src/app/version-list/version-list.component.ts
@@ -1,22 +1,62 @@
 import { Component, ViewEncapsulation, OnInit, ViewChild, AfterViewInit } from '@angular/core';
 import { ReleaseConvertorService } from './version-list-convertor/releases-convertor.service'
 import { Version } from './version-list-convertor/version.types';
+import {
+    trigger,
+    style,
+    state,
+    animate,
+    transition
+  } from '@angular/animations';
+
+  // -- animation triggers --
+    const rotate = trigger('rotatedState', [
+    state('default', style({ transform: 'rotate(0)' })),
+    state('rotated', style({ transform: 'rotate(-360deg)' })),
+    transition('rotated => default', animate('0.4s ease-out')),
+    transition('default => rotated', animate('0.4s ease-in'))
+  ])
+
+  const fadeInOut = trigger('fadeInOut', [
+    state(
+      'in',
+      style({
+        opacity: 1,
+      })
+    ),
+    transition('void => *', [style({ opacity: 0 }), animate('0.5s ease-out')]),
+    transition('* => void', [animate('0.1s ease-out'), style({ opacity: 0 })]),
+  ]);
+  // -- animation triggers --
 
 @Component({
     selector     : 'version-list',
     templateUrl  : './version-list.component.html',
     providers: [ReleaseConvertorService],
+    animations: [fadeInOut,rotate],
     encapsulation: ViewEncapsulation.None
 })
 export class VersionListComponent implements OnInit, AfterViewInit{
 
     releaseVersions!: Version[];
-    showFiller:boolean;
+    selectedVersion!:Version;
+
+    // -- animation controls --
+    isShown:boolean = true;
+    sidenavShow:boolean = true;
+    rotateState: string = 'default';
+    // -- animation controls --
 
     constructor(private versionConverterService:ReleaseConvertorService)
     {
-        this.showFiller = true;
         this.versionConverterService.getData();
+        
+    }
+
+    drawerToggle()
+    {
+        this.sidenavShow = !this.sidenavShow;
+        this.rotate();
     }
 
     ngAfterViewInit(): void {
@@ -25,13 +65,39 @@ export class VersionListComponent implements OnInit, AfterViewInit{
 
     async ngOnInit(): Promise<void>{
 
-        await this.versionConverterService.VersionsSubject$.subscribe(async (message) => {
+        this.versionConverterService.VersionsSubject$.subscribe(async (message) => {
             this.releaseVersions = message;
+            this.selectedVersion = this.releaseVersions[0];
         });
 
 	}
 
-    
+    public selectVersion(index:number)
+    {
+        this.selectedVersion = this.releaseVersions[index];
+        this.isShown = !this.isShown;
+        console.log(this.selectedVersion);
+    }
 
+
+    // -- animation methods --
+    rotate() {
+        this.rotateState = (this.rotateState === 'default' ? 'rotated' : 'default');
+    }
+
+    fadeInOut(): void {
+        this.isShown = !this.isShown;
+    }
+
+
+    onAnimationDone(event: any) {
+        console.log('onAnimationDone', event);
+        if(this.isShown == false)
+        {
+            this.isShown = true;
+        }
+    }
+
+     // -- animation methods --
 
 }
